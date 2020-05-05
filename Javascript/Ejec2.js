32749046dcb0abc828157e367ac730f956abe833
miguelortizm <mortiz@davivienda.com>
2020-05-05 07:50:58 -0500

    version2

---
+++ Javascript/Ejec2.js
@@ -0,0 +1,67 @@
+//variable entera
+var mivariableentera;
+mivariableentera = 1;
+//varialbe cadena
+var mivariablecadena = 'Miguel';
+mivariablecadena=1212;
+
+//variable flotante
+var mifloat = 10.2;
+
+var miVariable = '2';
+
+//con var se definen todo
+
+//let mifloat = 10.2; //no lo leen todos los navegadores
+
+var a = 2;
+var b = 3;
+var resultado = a+b;
+
+//alwer(resultado);
+console.log(a, b, resultado);
+console.log("varialbe a: " + a + "variable b: " + b + "resultado " + resultado);
+
+var MiArreglo = [1,2,3,'a','b']
+console.log(MiArreglo);
+
+
+console.log("Hola" + 2);
+
+var c = 2;
+var d = 1;
+
+var x = 4;
+var y = 5;
+
+/*
+if(a==b && x==y){
+console.log('Son iguales')
+}else{
+    console.log('No son iguales')
+}
+*/
+
+if(a==b ){
+    console.log('Son iguales')
+    }else if(b>y){
+        console.log('b es mayor que y')
+    }
+else{
+
+}
+
+var miarrayEjemplo =['a','b','c'] 
+//var indice = 0;
+for (var indice = 0; indice < miarrayEjemplo.length; indice ++){
+  console.log(indice);
+}
+
+
+//funciones
+function elNombreDeLaFucnion(){
+//Todo el contenido de la fucnion
+/*lo que
+quiero cmoentar comentario multilinea */
+}
+
---
+++ Javascript/Tarea1j.js
@@ -0,0 +1,110 @@
+
+var miArrTarea1 =[3,54,34,1,0,2,12,29,5] 
+var posicion = 0
+var posicion2 = 0
+var resultado
+var valor
+var valormayor
+var valormenor
+var indice = 0
+
+
+    valor= miArrTarea1[indice];
+    posicion2 = 0;
+    valormayor= miArrTarea1[posicion2];
+
+    //busca el mayor
+    while (posicion2 <= miArrTarea1.length){
+
+        valor2=  miArrTarea1[posicion2 + 1]
+        posicion2 = posicion2 + 1;
+
+        if(valor2 > valormayor){
+                    
+           valormayor = valor2;
+           valor2 = miArrTarea1[posicion2 + 1];
+
+          }
+          else{
+          }
+    
+
+    }
+
+    resultado = 'Valor mayor: ' + valormayor;
+    console.log(resultado);
+
+    
+    valor= miArrTarea1[indice];
+    posicion2 = 0
+    valormenor= miArrTarea1[posicion2];
+
+
+    //busca el menor
+    while (posicion2 <= miArrTarea1.length){
+
+        valor2=  miArrTarea1[posicion2 + 1]
+        posicion2 = posicion2 + 1;
+
+        if(valor2 < valormenor){
+                    
+           valormenor = valor2;
+           valor2 = miArrTarea1[posicion2 + 1];
+
+          }
+          else{
+          }
+    
+
+    }
+
+    resultado = 'Valor menor: ' + valormenor;
+    console.log(resultado);
+
+
+
+    posicion2 = 0
+    valor= miArrTarea1[posicion2];
+
+
+    resultado =  valormenor ;
+    console.log(resultado);
+
+        while (valormenor != valormayor){
+            
+            posicion2 = 0
+            //valor = miArrTarea1[posicion2]; 
+            valor = valormayor; 
+
+            while (posicion2 <= miArrTarea1.length){
+                    
+                    
+                       
+                    valor2=  miArrTarea1[posicion2 + 1]
+                    posicion2 = posicion2 + 1;        
+//validar mayor valor
+                    if( valor2 > valormenor){
+
+                        if(valor2 < valor){
+
+                            valor = valor2;
+                            valor2 = miArrTarea1[posicion2 + 1];
+ 
+                           }
+                           else{
+                           }
+                       
+                      }
+                      else{
+                      }
+                      
+                   
+            }
+            valormenor = valor;
+            resultado =  valormenor ;
+            console.log(resultado);
+        }
+
+
+
+
